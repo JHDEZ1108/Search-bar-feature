6183e5e6e9a4bf82132ba1e4770453df5cfef1a7
Josué Hernández <joheca1998@outlook.com>
2022-12-20 09:53:44 -0600

    Agregando primeros estilos del buscador

--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Stack, TextField, IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searcher= () => {
   return(
-    <Stack>
-      <TextField />
-      <IconButton />
+    <Stack 
+    direction = 'row' 
+    sx={{
+      marginTop: '30px',
+      width: '80%'
+    }}>
+      <TextField 
+      id="filled-basic" label="Github User" variant="filled" 
+      color="primary"
+      placeholder="Search user"
+      size="small"
+      sx={{
+        width: '100%',
+      }}
+      />
+      <IconButton sx={{
+        left: '-50px'
+      }}>
+        <SearchIcon />
+      </IconButton>
     </Stack>
   )
 }
