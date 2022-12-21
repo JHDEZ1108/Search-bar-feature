6ecf7a14e7700ef7c539da66c6897d9f9461c5cc
Josué Hernández <joheca1998@outlook.com>
2022-12-21 11:45:47 -0600

    Conectando a la API de Github

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@mui/material"
 import Searcher from "@components/Searcher";
+import { getUser } from "../services/users"
 
 const App = () => {
 
   const [inputUser, setInputUser] = useState('JHDEZ1108');
   const [userState, setState] = useState('inputUser');
+  
+  const getGitHubUser =  async (user) =>{
+    const userResponse = await getUser(user);
+  };
+  
+  useEffect(() => {
+    getGitHubUser(inputUser);
+  });
   
   return(
     <Container sx={{
