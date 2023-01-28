0aac795841438a4911fd5c8a92472996a3bb4159
Josué Hernández <joheca1998@outlook.com>
2023-01-28 17:36:37 -0600

    corrigiendo typos

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles/global.scss';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
+  <App />
+);
