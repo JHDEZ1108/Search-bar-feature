f16be2cbd1d7fbe19f5f400f97dc9242c9f02786
Josué Hernández <joheca1998@outlook.com>
2023-01-30 10:31:35 -0600

    Corrigiendo error

--- src/components/PrincipalInformation/index.jsx
+++ src/components/PrincipalInformation/index.jsx
@@ -8,14 +8,12 @@ const PrincipalInformation = (props) => {
   const { 
     name,
     login,
-    created_at,
   } = userState;
 
   return(
     <Fragment>
         <Stack direction="row" sx={{justifyContent: 'space-between'}}>
           <Typography variant="h4">{name}</Typography>
-          <Typography variant="subtitle2">{created_at}</Typography>
       </Stack>
       <Typography variant="caption">{`@${login}`}</Typography>
     </Fragment>
